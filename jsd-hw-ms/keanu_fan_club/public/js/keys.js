54bb314ef32eb3b24335daa5c4d96a62b45f4e54
Matthew Spelman <matthewspelman@gmail.com>
2018-04-18 21:02:11 -0400

    Class 15 Keanu Fan Club

---
+++ jsd-hw-ms/keanu_fan_club/public/js/keys.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyABQ3MKJzWo-MOT9cTlzpml9UoOEArJ_xw",
+    authDomain: "jsd-keanu-project-4cc5c.firebaseapp.com",
+    databaseURL: "https://jsd-keanu-project-4cc5c.firebaseio.com",
+    projectId: "jsd-keanu-project-4cc5c",
+    storageBucket: "jsd-keanu-project-4cc5c.appspot.com",
+    messagingSenderId: "648523329342"
+	};
+firebase.initializeApp(config);
